613445d2d80f212e9601c319203243fa79a8bf94
Vinoth Kumar V <vinoth.kumar@zerodha.com>
2020-01-23 01:06:40 +0530

    multiselect unwanted features removed

--- src/components/MultiSelect.vue
+++ src/components/MultiSelect.vue
@@ -1,222 +1,174 @@
 <template>
-    <div :style="containerStyle" class="multiselect">
-        <div class="multiselect__select" :style="inputStyle" @click="keyCapture">
-            <p v-show="false"> {{ currItem }}</p>
-            <ul class="multiselect__tags">
-                <li class="multiselect__tag" v-for="data in selectedValues" :key="data.label">
-									{{ data.label }}
-									<span class="close" @click="clearItem($event, data)">&times;</span>
-								</li>
-                <li class="multiselect__tag">
-                    <input type="text" class="search" ref="listsearch" :placeholder="placeholder" v-model="search" @focus="onFocus" @keydown="keyMonitor"></input>
-                </li>
-            </ul>
-        </div>
-        <ul v-show="showLabels" class="multiselect__content" id="labels" :style="labelStyle">
-            <li class="multiselect__content--elem" @keyup="navOnLabel" @keypress="onEnterPress" v-for="(data, index) in filteredOptions" :key="data.name" :class="{navcolor: data.navigate, selected: data.selected}" @click="onLabelClick(data, index)"> {{ data.label }} </li>
-        </ul>
+  <div :style="containerStyle" class="multiselect">
+    <div class="multiselect__select" :style="inputStyle" @click="keyCapture">
+      <ul class="multiselect__tags">
+        <li class="multiselect__tag" v-for="data in selectedValues" :key="data.label">
+          {{ data.label }}
+          <span class="close" @click="clearItem(data)">&times;</span>
+        </li>
+        <li class="multiselect__tag">
+          <input type="text" class="search" ref="listsearch" :placeholder="placeholder" v-model="search" @focus="onFocus" @keydown="keyMonitor" />
+        </li>
+      </ul>
     </div>
+    <ul v-show="showLabels" class="multiselect__content" id="labels" :style="labelStyle">
+      <li class="multiselect__content--elem" v-for="(data, index) in filteredOptions" :key="data.name" :class="{navcolor: data.navigate, selected: data.selected}" @click="onLabelClick(data, index)"> {{ data.label }} </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-	name: 'su-multi-select',
-	data () {
-		return {
-			showLabels: false,
-			search: '',
-			currItem: 1,
-			divHeight: 0
-		}
-	},
-	props: {
-		options: {
-			type: [Object, Array],
-			required: true
-		},
-		trackBy: {
-			type: String,
-			required: false
-		},
-		width: {
-			type: [Number, String],
-			required: false
-		},
-		label: {
-			type: String,
-			required: false
-		},
-		selectedValues: {
-			type: [Object, Array],
-			required: false
-		}
+  name: 'multi-select',
+  data() {
+    return {
+      showLabels: false,
+      search: '',
+      divHeight: 0
+    }
+  },
+  props: {
+    options: {
+      type: [Object, Array],
+      required: true
     },
-    // if any values list of objects present in selected values, then those values are selected..
-	beforeMount () {
-		this.options.map((item, index) => {
-			item.navigate = false
-			item.itemId = index + 1
-			if (this.selectedValues.map(val => val.name).indexOf(item.name) > -1) {
-				item.selected = true
-			} else {
-				item.selected = false
-			}
-		})
-	},
-	created () {
-		this.heightChange()
-	},
-	updated () {
-		this.heightChange()
-	},
-	mounted () {
-		document.addEventListener("keyup", this.navOnLabel)
-		document.addEventListener("keypress", this.onEnterPress)
-		window.addEventListener("click", this.onSelectContainer)
-	},
-	destroyed () {
-		document.removeEventListener("keyup", this.navOnLabel)
-		document.removeEventListener("keypress", this.onEnterPress)
-		window.removeEventListener("click", this.onSelectContainer)
-	},
-	methods: {
-        // on click of the multi select element we need to show the dropdown, or hide it..
-		onSelectContainer (event) {
-            if (document.getElementsByClassName('multiselect')[0].contains(event.target)) {
-				this.showLabels = true
-			} else {
-                this.showLabels = false
-            }
-        },
-        // push new item to the array list
-        addItem (array, item) {
-            let sValue = {}
-            sValue[this.trackBy] = item[this.trackBy]
-            sValue[this.label] = item[this.label]
-            array.push(sValue)
-            return array
-        },
-        // remove item from the array list
-        removeItem (array, item) {
-            array.splice(array.map(val => val[this.trackBy]).indexOf(item[this.trackBy]), 1)
-        },
-		keyMonitor (event) {
-			let evtkey = event.keyCode
-			let valLength = this.selectedValues.length
-			if ((evtkey >= 48 && evtkey <= 57) || (evtkey >= 65 && evtkey <= 90) || (evtkey >= 97 && evtkey <= 122) || (evtkey == 8)) {
-				if (this.search.length <= 0 && evtkey == 8 && valLength > 0) {
-					for (var key in this.filteredOptions) {
-						if (this.filteredOptions[key][this.trackBy].indexOf(this.selectedValues[valLength -1][this.trackBy]) > -1) {
-							this.selectedValues.splice((valLength-1), 1)
-							this.filteredOptions[key].selected = !this.filteredOptions[key].selected
-							break
-						}
-					}
-				}
-				this.$nextTick(() => {
-					this.search = event.target.value
-					this.currItem = 1
-				})
-			}
-        },
-        // based on selected values height of the box varies..
-		heightChange () {
-			let str = this.selectedValues.join()
-			this.divHeight = (Math.ceil((str.length+1) / 45)) * 40
-        },
-        // on select of the multi select container it needs to focus on the input field..
-		keyCapture () {
-			this.$nextTick(() => {
-				this.$refs.listsearch.focus()
-			})
-		},
-		// Deselects or removes the element from the multi-select div when clicking the close button...
-		clearItem (event, item) {
-            this.removeItem(this.selectedValues, item)
-            item.selected = !item.selected
-        },
-        // show labels list on focus of the multi select container..
-		onFocus () {
-			this.showLabels = true
-		},
-		// Selects or append the elements to the multi-select div when clicking on the list elements...
-		onLabelClick (data, index) {
-            data.selected = !data.selected
-            // this.showLabels = true
-            this.search = ''
-            this.filteredOptions.map(item => {
-                if (item[this.trackBy].toLowerCase().indexOf(data[this.trackBy].toLowerCase()) > -1 && item.selected == true) {
-                    this.addItem(this.selectedValues, item)
-                } else if (item[this.trackBy].toLowerCase().indexOf(data[this.trackBy].toLowerCase()) > -1 && item.selected == false) {
-                    this.removeItem(this.selectedValues, data)
-                }
-            })
-		},
-		// Naviate through label items on key up & down
-		navOnLabel () {
-			let getLabel = document.getElementById('labels')
-			if (event.keyCode == 38) {
-				if (this.currItem !== 1) {
-					this.currItem--
-					getLabel.scrollBy(0, -20)
-				} else {
-					this.currItem = this.currItem + this.filteredOptions.length-1
-					getLabel.scrollBy(0, (20*this.filteredOptions.length-1))
-				}
-			} else if (event.keyCode == 40) {
-				if (this.currItem !== this.filteredOptions.length) {
-					this.currItem++
-					getLabel.scrollBy(0, 20)
-				} else {
-					this.currItem = this.currItem - (this.filteredOptions.length-1)
-					getLabel.scrollBy(0, (-20*this.filteredOptions.length-1))
-				}
-			}
-			this.filteredOptions.map((item, index) => {
-				if (item[this.trackBy] == this.filteredOptions[this.currItem-1][this.trackBy]) {
-					item.navigate = true
-				} else {
-					item.navigate = false
+    trackBy: {
+      type: String,
+      required: false
+    },
+    width: {
+      type: [Number, String],
+      required: false
+    },
+    label: {
+      type: String,
+      required: false
+    },
+    selectedValues: {
+      type: [Object, Array],
+      required: false
+    },
+    placeholder: {
+      type: String,
+      required: false
+    }
+  },
+  // if any values list of objects present in selected values, then those values are selected..
+  beforeMount() {
+    this.options.map((item, index) => {
+      item.navigate = false
+      item.itemId = index + 1
+      if (this.selectedValues.map(val => val.name).indexOf(item.name) > -1) {
+        item.selected = true
+      } else {
+        item.selected = false
+      }
+    })
+  },
+  created() {
+    this.heightChange()
+  },
+  updated() {
+    this.heightChange()
+  },
+  mounted() {
+    window.addEventListener("click", this.onSelectContainer)
+  },
+  destroyed() {
+    window.removeEventListener("click", this.onSelectContainer)
+  },
+  methods: {
+    // based on selected values height of the box varies..
+    heightChange() {
+      this.divHeight = (Math.ceil((this.selectedValues.join().length + 1) / 45)) * 40
+    },
+    // show labels list on focus of the multi select container..
+    onFocus() {
+      this.showLabels = true
+    },
+    // on click of the multi select element we need to show the dropdown, or hide it..
+    onSelectContainer(event) {
+      if (document.querySelector('.multiselect').contains(event.target)) {
+        this.showLabels = true
+      } else {
+        this.showLabels = false
+      }
+    },
+    // push new item to the array list
+    addItem(array, item) {
+      let sValue = {
+        [this.trackBy]: item[this.trackBy],
+        [this.label]: item[this.label]
+      }
+      return array.push(sValue)
+    },
+    // remove item from the array list
+    removeItem(array, item) {
+      array.splice(array.map(val => val[this.trackBy]).indexOf(item[this.trackBy]), 1)
+    },
+    // Deselects or removes the element from the multi-select div when clicking the close button...
+    clearItem(selectedItem) {
+      this.filteredOptions.map((item) => {
+				if (item[this.trackBy].indexOf(selectedItem[this.trackBy]) > -1) {
+          this.removeItem(this.selectedValues, selectedItem)
+					item.selected = !item.selected
 				}
 			})
-		},
-		// Select & Deselect Labels on keypress Enter
-		onEnterPress () {
-			if (event.keyCode == 13) {
-                var filteredCurrentOption = this.filteredOptions[this.currItem - 1]
-				if (filteredCurrentOption.selected === false) {
-                    this.addItem(this.selectedValues, filteredCurrentOption)
-					filteredCurrentOption.selected = true
-					this.search = ''
-					// this.currItem = 1
-				} else {
-                    filteredCurrentOption.selected = false
-                    this.removeItem(this.selectedValues, filteredCurrentOption)
-				}
-			}
-		}
-	},
-	computed: {
-		// Filter Lists based upon search
-		filteredOptions () {
-			return this.options.filter(item => {
-				return item.label.toLowerCase().indexOf(this.search.toLowerCase()) > -1
-			})
-		},
-		// Conditional style rendering
-		containerStyle () {
-			return { width: (this.width !== undefined ? this.width+'px': '300px') }
-		},
-		inputStyle () {
-			return { height: this.divHeight+'px' }
-		},
-		iconStyle () {
-			return { left: '335px', position: 'absolute', top: (this.showLabels) ? '18px' : '10px' }
-		},
-		labelStyle () {
-			return { position: 'absolute', width: (this.width !== undefined ? this.width+'px': '300px') }
-		}
-	}
+    },
+    keyMonitor(event) {
+      /* eslint-disable no-console */
+      let valLength = this.selectedValues.length
+      if (valLength > 0 && event.keyCode == 8) {
+        this.clearItem(this.selectedValues[valLength - 1])
+      }
+      this.$nextTick(() => {
+        this.search = event.target.value
+      })
+    },
+    // on select of the multi select container it needs to focus on the input field..
+    keyCapture() {
+      this.$nextTick(() => {
+        this.$refs.listsearch.focus()
+      })
+    },
+    // Selects or append the elements to the multi-select div when clicking on the list elements...
+    onLabelClick(data) {
+      data.selected = !data.selected
+      this.search = ''
+      this.filteredOptions.map(item => {
+        if (item[this.trackBy].toLowerCase().indexOf(data[this.trackBy].toLowerCase()) > -1 && item.selected == true) {
+          this.addItem(this.selectedValues, item)
+        } else if (item[this.trackBy].toLowerCase().indexOf(data[this.trackBy].toLowerCase()) > -1 && item.selected == false) {
+          this.removeItem(this.selectedValues, data)
+        }
+      })
+    }
+  },
+  computed: {
+    // Filter Lists based upon search
+    filteredOptions() {
+      return this.options.filter(item => {
+        return item[this.label].toLowerCase().indexOf(this.search.toLowerCase()) > -1
+      })
+    },
+    // Conditional style rendering
+    containerStyle() {
+      return {
+        width: (this.width !== undefined ? this.width + 'px' : '300px')
+      }
+    },
+    inputStyle() {
+      return {
+        height: this.divHeight + 'px'
+      }
+    },
+    labelStyle() {
+      return {
+        position: 'absolute',
+        width: (this.width !== undefined ? this.width + 'px' : '300px')
+      }
+    }
+  }
 }
 </script>
 
